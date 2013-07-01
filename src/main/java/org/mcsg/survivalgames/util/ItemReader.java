8f4133a066226356b39860721e68c13a3e5e3c54
Max <maxpowa1@gmail.com>
2013-07-01 17:25:21 -0600

    Add official names to enchantment option in kits

--- src/main/java/org/mcsg/survivalgames/util/ItemReader.java
+++ src/main/java/org/mcsg/survivalgames/util/ItemReader.java
@@ -24,10 +24,42 @@ public class ItemReader {
 			encids.put(e.toString().toLowerCase().replace("_", ""), e);
 		}
 		
+		//Anything enchants
+		encids.put("unbreaking", Enchantment.DURABILITY);
 		
+		//Armor Enchants
+		encids.put("prot", Enchantment.PROTECTION_ENVIRONMENTAL);
+		encids.put("protection", Enchantment.PROTECTION_ENVIRONMENTAL);
+		encids.put("fireprot", Enchantment.PROTECTION_FIRE);
+		encids.put("fireprotection", Enchantment.PROTECTION_FIRE);
+		encids.put("featherfall", Enchantment.PROTECTION_FALL);
+		encids.put("featherfalling", Enchantment.PROTECTION_FALL);
+		encids.put("blastprot", Enchantment.PROTECTION_EXPLOSION);
+		encids.put("blastprotection", Enchantment.PROTECTION_EXPLOSION);
+		encids.put("projectileprot", Enchantment.PROTECTION_PROJECTILE);
+		encids.put("projectileprotection", Enchantment.PROTECTION_PROJECTILE);
+		encids.put("aquaaffinity", Enchantment.WATER_WORKER);
+		encids.put("respiration", Enchantment.OXYGEN);
+		
+		//Weapon Enchants
+		encids.put("smite", Enchantment.DAMAGE_UNDEAD);
+		encids.put("baneofarthropods", Enchantment.DAMAGE_ARTHROPODS)
 		encids.put("sharpness", Enchantment.DAMAGE_ALL);
 		encids.put("dmg", Enchantment.DAMAGE_ALL);
 		encids.put("fire", Enchantment.FIRE_ASPECT);
+		encids.put("looting", Enchantment.LOOT_BONUS_MOBS);
+		encids.put("loot", Enchantment.LOOT_BONUS_MOBS);
+		
+		//Tool enchants (Silk Touch's enchantment name is Silk_Touch, so it's covered above)
+		encids.put("efficiency", Enchantment.DIG_SPEED);
+		encids.put("fort", Enchantment.LOOT_BONUS_BLOCKS);
+		encids.put("fortune", Enchantment.LOOT_BONUS_BLOCKS);
+		
+		//Bow specific enchants
+		encids.put("punch", Enchantment.ARROW_KNOCKBACK);
+		encids.put("power", Enchantment.ARROW_DAMAGE);
+		encids.put("infinity", Enchantment.ARROW_INFINITE);
+		encids.put("flame", Enchantment.ARROW_FIRE);
 
 	}
 	
