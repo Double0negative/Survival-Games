2be7f0a2d4a5dc3cdf62821cbfd64e6dda6a489d
soulofwolf <web_wizardz@mail.com>
2013-07-31 01:03:15 -0500

    kit costs reduction added

--- src/main/java/org/mcsg/survivalgames/GameManager.java
+++ src/main/java/org/mcsg/survivalgames/GameManager.java
@@ -172,6 +172,16 @@ public class GameManager {
 		if(i <= kits.size()){
 			Kit k = getKits(p).get(i);
 			if(k!=null){
+                if (k.getCost() > 0){
+                    if (SurvivalGames.econOn){
+                        if (SurvivalGames.econ.getBalance(p.getName()) < k.getCost()){
+                            msgmgr.sendMessage(MessageManager.PrefixType.WARNING, "You can not afford to buy this kit!", p);
+                            return;
+                        }
+                        SurvivalGames.econ.withdrawPlayer(p.getName(), k.getCost());
+                        msgmgr.sendMessage(MessageManager.PrefixType.INFO, k.getCost() + " have been withdrawn from your funds.", p);
+                    }
+                }
 				p.getInventory().setContents(k.getContents().toArray(new ItemStack[0]));
 			}
 		}
