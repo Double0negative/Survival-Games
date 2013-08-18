82f76f486a4499ad266a0e9cfbf6edcee988370b
hitech95 <nicveronese@gmail.com>
2013-08-18 12:48:44 +0200

    Added per Arena Whitelist/BlackList (optional)
    Some changes on Arena.java and Game.java
    Changed some event: PlayerBreak and PlayerPlace
    
    If a person insert -1 on whitelist/blacklist all block are
    allowed/locked
    
    Games whithout optional configuration use the general config.

--- src/main/java/org/mcsg/survivalgames/Arena.java
+++ src/main/java/org/mcsg/survivalgames/Arena.java
@@ -1,32 +1,74 @@
 package org.mcsg.survivalgames;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Location;
 
 public class Arena {
 
     Location min;
     Location max;
+    
+    boolean isWhite = true;
+    boolean useGeneral;
+    ArrayList<Integer> allowedBreak = new ArrayList<Integer>();
+    ArrayList<Integer> allowedPlace = new ArrayList<Integer>();
 
-    public Arena(Location min, Location max) {
+    public Arena(Location min, Location max, List<Integer> allowedBreak, List<Integer> allowedPlace) {
         this.max = max;
         this.min = min;
+        this.allowedBreak.addAll(allowedBreak);
+        this.allowedPlace.addAll(allowedPlace);
+        this.useGeneral = false;
+    }
+
+    public Arena(Location min, Location max, List<Integer> allowedBreak, List<Integer> allowedPlace, boolean isWhite) {
+        this(min, max, allowedBreak, allowedPlace);
+        this.isWhite = isWhite;
+    }
+    
+    public Arena(Location min, Location max) {
+        this(min, max, new ArrayList<Integer>(), new ArrayList<Integer>());
         
+        useGeneral = true;
     }
 
     public boolean containsBlock(Location v) {
-        if (v.getWorld() != min.getWorld()) return false;
+        if (v.getWorld() != min.getWorld()) {
+            return false;
+        }
         final double x = v.getX();
         final double y = v.getY();
         final double z = v.getZ();
-        return x >= min.getBlockX() && x < max.getBlockX() + 1 && y >= min.getBlockY() && y < max.getBlockY() + 1 && z >= min.getBlockZ() && z < max.getBlockZ() + 1;  
+        return x >= min.getBlockX() && x < max.getBlockX() + 1 && y >= min.getBlockY() && y < max.getBlockY() + 1 && z >= min.getBlockZ() && z < max.getBlockZ() + 1;
     }
 
     public Location getMax() {
-    	Runtime.getRuntime().freeMemory();
+        Runtime.getRuntime().freeMemory();
         return max;
     }
 
     public Location getMin() {
         return min;
+    }
+    
+    public boolean useGeneral(){
+        return this.useGeneral;
+    }
+
+    public boolean canPlace(int id) {
+        if (allowedPlace.contains(-1)) {
+            return true;
+        } else {
+            return (isWhite) ? allowedPlace.contains(id) : !allowedPlace.contains(id);
+        }
+    }
+
+    public boolean canBreak(int id) {
+        if (allowedBreak.contains(-1)) {
+            return true;
+        } else {
+            return (isWhite) ? allowedBreak.contains(id) : !allowedBreak.contains(id);
+        }
     }
 }
