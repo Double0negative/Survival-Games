49d322e35e6e6adeef421ad30530d6dd904f5936
Toby <toby.scrace@gmail.com>
2013-09-03 03:50:13 +0100

    Prevent a NumberFormatException when the supplied AddWall argument isn't a number.
    Also some formatting.

--- src/main/java/org/mcsg/survivalgames/commands/AddWall.java
+++ src/main/java/org/mcsg/survivalgames/commands/AddWall.java
@@ -6,21 +6,26 @@ import org.mcsg.survivalgames.MessageManager;
 import org.mcsg.survivalgames.SettingsManager;
 
 
-
-public class AddWall implements SubCommand{
+public class AddWall implements SubCommand {
 
     @Override
     public boolean onCommand(Player player, String[] args) {
         if(!player.hasPermission(permission()) && !player.isOp()){
             MessageManager.getInstance().sendFMessage(MessageManager.PrefixType.ERROR, "error.nopermission", player);
             return true;
-        }
-        else if(args.length<1){
+        } else if (args.length < 1){
                 MessageManager.getInstance().sendFMessage(MessageManager.PrefixType.ERROR, "error.notspecified", player, "input-Arena");
         	return true;
         }
-       LobbyManager.getInstance().setLobbySignsFromSelection(player, Integer.parseInt(args[0]));
-       return true;
+        int arena;
+        try {
+        	arena = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e){
+        	MessageManager.getInstance().sendFMessage(MessageManager.PrefixType.ERROR, "error.notanumber", player, "input-Arena");
+        	return true;
+        }
+        LobbyManager.getInstance().setLobbySignsFromSelection(player, arena);
+        return true;
     }
 
     @Override
@@ -28,10 +33,10 @@ public class AddWall implements SubCommand{
         return "/sg addwall <id> - " + SettingsManager.getInstance().getMessageConfig().getString("messages.help.addwall", "Add a lobby stats wall for Arena <id>");
     }
 
-	@Override
-	public String permission() {
-		return "sg.admin.addwall";
-	}
+    @Override
+    public String permission() {
+        return "sg.admin.addwall";
+    }
 
-    //TODO: TAKE A W.E SELECTIONA AND SET THE LOBBY. ALSO SET LOBBY WALL
+    //TODO: TAKE A W.E SELECTION AND SET THE LOBBY. ALSO SET LOBBY WALL
 }
